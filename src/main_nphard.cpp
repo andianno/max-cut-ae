6614a6bc90b9d560af5aea5fc020bed4aac65c0b
andianno <andrea.iannotti@student.univaq.it>
2026-05-31 10:07:29 +0200

    Refactor code structure for improved readability and maintainability

--- src/main_nphard.cpp
+++ src/main_nphard.cpp
@@ -10,7 +10,7 @@ int main() {
 
     // ATTENZIONE: Dimensioni piccolissime per l'Algoritmo Esatto
     // Già a 30 nodi potrebbe metterci diversi secondi o minuti.
-    vector<int> sizes = {10, 15, 20, 25, 28}; 
+    vector<int> sizes = {10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28}; 
     double density = 0.5; // Grafo molto denso per metterlo sotto stress
     int iterazioni_random = 100;
 
